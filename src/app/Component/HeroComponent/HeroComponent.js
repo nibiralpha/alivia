78a58297e662347e02056308d6dd6c01aefc25c1
Nibir Alam <nibiralpha@gmail.com>
2026-03-23 04:51:52 +0600

    change css file name

--- src/app/Component/HeroComponent/HeroComponent.js
+++ src/app/Component/HeroComponent/HeroComponent.js
@@ -1,32 +1,32 @@
-import "./Hero.css";
+import styles from "./Hero.module.css";
 
 export default function HeroComponent() {
   return (
-    <div className="t_container">
-      <div className="menu_container">
-        <div className="logo">Alivia</div>
-        <div className="menu">
-          <div className="line"></div>
-          <div className="line"></div>
+    <div className={styles.t_container}>
+      <div className={styles.menu_container}>
+        <div className={styles.logo}>Alivia</div>
+        <div className={styles.menu}>
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
         </div>
       </div>
       
-      <div className="about_text_container">
-        <div className="about_text">
-          <div className="left_side">
-            <div className="left_side_text">Branding / Deisng / UI/UX / Strategy /</div>
+      <div className={styles.about_text_container}>
+        <div className={styles.about_text}>
+          <div className={styles.left_side}>
+            <div className={styles.left_side_text}>Branding / Deisng / UI/UX / Strategy /</div>
           </div>
-          <div className="right_side">
-            <img className="right_side_img" src="/images/world.svg" />
-            <div className="right_side_text">Based in Amsterdam the Dutch capital</div>
+          <div className={styles.right_side}>
+            <img className={styles.right_side_img} src="/images/world.svg" />
+            <div className={styles.right_side_text}>Based in Amsterdam the Dutch capital</div>
           </div>
         </div>
       </div>
 
-      <div className="alivia_text_container">
-        <div className="alivia_left_text">Hi, I am</div>
-        <div className="alivia_text">Alivia</div>
-        <div className="alivia_right_text">Daiana</div>
+      <div className={styles.alivia_text_container}>
+        <div className={styles.alivia_left_text}>Hi, I am</div>
+        <div className={styles.alivia_text}>Alivia</div>
+        <div className={styles.alivia_right_text}>Daiana</div>
       </div>
     </div>
   );
