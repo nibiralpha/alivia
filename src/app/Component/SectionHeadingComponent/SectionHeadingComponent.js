e52b313b1b4e509ed765d92bae51fd58c44132ef
Nibir Alam <nibiralpha@gmail.com>
2026-03-30 16:32:10 +0600

    section heading component animation added

--- src/app/Component/SectionHeadingComponent/SectionHeadingComponent.js
+++ src/app/Component/SectionHeadingComponent/SectionHeadingComponent.js
@@ -1,4 +1,14 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import styles from "./Heading.module.css";
+
+// Register the plugin
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger);
+}
 
 export default function SectionHeadingComponent({
   name,
@@ -6,8 +16,34 @@ export default function SectionHeadingComponent({
   mbottom,
   mtop,
 }) {
+  const headingRef = useRef(null);
+
+  useEffect(() => {
+    const el = headingRef.current;
+
+    gsap.fromTo(
+      el,
+      {
+        y: 100,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: el,
+          start: "top 98%",
+          toggleActions: "play none none none",
+        },
+      },
+    );
+  }, []);
+
   return (
     <div
+      ref={headingRef}
       style={{
         marginTop: mtop,
         paddingBottom: mbottom,
