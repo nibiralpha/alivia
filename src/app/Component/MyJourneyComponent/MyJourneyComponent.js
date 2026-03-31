6f00e5ab6143e10e21dbee828fb0bbed77ecb69e
Nibir Alam <nibiralpha@gmail.com>
2026-03-31 14:41:54 +0600

    my journey section animation added

--- src/app/Component/MyJourneyComponent/MyJourneyComponent.js
+++ src/app/Component/MyJourneyComponent/MyJourneyComponent.js
@@ -1,9 +1,43 @@
-import SectionHeadingComponent from "../SectionHeadingComponent/SectionHeadingComponent";
+"use client";
+
+import { useEffect, useRef } from "react";
 import styles from "./MyJourneyComponent.module.css";
 
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger);
+}
+
 export default function ProjectComponent() {
+  const journeyRef = useRef(null);
+
+  useEffect(() => {
+    const journeySection = journeyRef.current;
+
+    gsap.fromTo(
+      journeySection,
+      {
+        y: 100,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: journeySection,
+          start: "top 98%",
+          toggleActions: "play none none none",
+        },
+      },
+    );
+  }, []);
+
   return (
-    <div className={styles.journey_component}>
+    <div ref={journeyRef} className={styles.journey_component}>
       <div className={styles.area}>
         <div className={styles.journey}>
           <img src="/images/journey.webp" />
