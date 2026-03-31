9066d57c6ce74cbbfebd7ba4f8628b2e9de30714
Nibir Alam <nibiralpha@gmail.com>
2026-03-31 15:42:39 +0600

    discuss section animate

--- src/app/Component/DiscussComponent/DiscussComponent.js
+++ src/app/Component/DiscussComponent/DiscussComponent.js
@@ -1,12 +1,70 @@
-import SectionHeadingComponent from "../SectionHeadingComponent/SectionHeadingComponent";
+"use client";
+
+import { useEffect, useRef } from "react";
 import styles from "./Discuss.module.css";
 
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger);
+}
+
 export default function ProjectComponent() {
+
+   const talkRef = useRef(null);
+   const discussRef = useRef(null);
+
+  useEffect(() => {
+    const talkSection = talkRef.current;
+    const discussSection = discussRef.current;
+
+    // Lets talk
+    gsap.fromTo(
+      talkSection,
+      {
+        y: 100,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: talkSection,
+          start: "top 98%",
+          toggleActions: "play none none none",
+        },
+      },
+    );
+
+    // Discuss button
+    gsap.fromTo(
+      discussSection,
+      {
+        y: 100,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: discussSection,
+          start: "top 98%",
+          toggleActions: "play none none none",
+        },
+      },
+    );
+  }, []);
+
   return (
     <div className={styles.discuss}>
       <div className={styles.start_section}>
-        <div className={styles.title}>Anything in Mind? Let’s Talk</div>
-        <div className={styles.discuss_button}>
+        <div ref={talkRef} className={styles.title}>Anything in Mind? Let’s Talk</div>
+        <div ref={discussRef} className={styles.discuss_button}>
           <div className={styles.button}>Let's Discuss</div>
         </div>
       </div>
